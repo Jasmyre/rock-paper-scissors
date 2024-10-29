1228aaac57373bfe0f5abcdff6718441e900f0ad
Jasmyre <lanuzajasmyreandrei@gmail.com>
2024-10-29 12:58:34 +0800

    Feat: Added conditional banner

--- history/index.js
+++ history/index.js
@@ -3,9 +3,19 @@ const { log } = console;
 const search = window.location.search;
 const queries = new URLSearchParams(search);
 
-const playerWonQuerry = queries.get("playerWon");
-const playerWon = (playerWonQuerry == "true") ? true : false;
+const playerWonQuerry = (queries.get("playerWon")) ? queries.get("playerWon") : null;
+const playerWon = (playerWonQuerry) ? ((playerWonQuerry == "true") ? true : false) : null;
 
-const banner = document.getElementById("winner-banner-text");
+const banner = document.getElementById("winner-banner");
 
-banner.innerText = (playerWon == true) ? "You Won!" : "You Lose.";
+const winnerTemplate = `<div class="winner-section-wrapper section-lg">
+					<h1>YOU WON!</h1>
+					<a href="../game/index.html">Play Again</a>
+				</div>`;
+const loserTemplate = `<div class="winner-section-wrapper section-lg">
+					<h1>YOU Lose.</h1>
+					<a href="../game/index.html">Play Again</a>
+				</div>`;
+
+banner.innerHTML = (playerWon == true) ? winnerTemplate : "";
+banner.innerHTML = (playerWon == false) ? loserTemplate : "";
